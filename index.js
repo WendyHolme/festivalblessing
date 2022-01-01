3abaf84f3105c9a5668cc80a8115dfac254969a6
WendyHolme <90240658+WendyHolme@users.noreply.github.com>
2022-01-01 16:35:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||小徐|祝你|中秋节|快乐|best wishes|#rectangle|');
+      S.UI.simulate('|#countdown 3||Happy|New Year|2022|best wishes|#rectangle|');
     }
 
     S.Drawing.loop(function () {
